eed49caa7f8e98f6480f066d89899b0f0608bedf
wazeem123 <mwazeem84@gmail.com>
2021-03-25 20:30:15 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='grid-container'>
+        <header className='row'>
+          <div>
+            <a className='brand' href='/'>
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href='/cart'>Cart</a>
+            <a href='/signin'>Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
+          <Route exact path='/' component={HomeScreen}></Route>
+          <Route path='/product/:id' component={ProductScreen}></Route>
+        </main>
+        <footer className='row center'>All rights reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
